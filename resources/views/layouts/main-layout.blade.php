0c294f308996cb915668813c72332da449fcd131
Vale <oirelav95@gmail.com>
2023-01-31 00:05:10 +0100

    modifico contenuto main-layout

--- resources/views/layouts/main-layout.blade.php
+++ resources/views/layouts/main-layout.blade.php
@@ -4,12 +4,15 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css">
     <title>DC Comics</title>
     @vite('resources/js/app.js')
 </head>
 <body>
     @include('components.header')
+
     @yield('content')
+
     @include('components.footer')
     
 </body>
