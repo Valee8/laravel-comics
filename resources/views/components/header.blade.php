8844d40f7bbf41e14c18802bc607df5085c9eca1
Vale <oirelav95@gmail.com>
2023-01-30 18:44:45 +0100

    creo file header.blade.php

---
+++ resources/views/components/header.blade.php
@@ -0,0 +1,75 @@
+<header>
+    <nav>
+        <div class="container">
+
+            <a href="/">
+                <img src="{{ Vite::asset('resources/img/dc-logo.png') }}" alt="Logo DC" class="logo">
+            </a>
+
+            <ul>
+                <li>
+                    <a href="/">
+                        Characters
+                    </a>
+                </li>
+
+                <li>
+                    <a href="/">
+                        Comics
+                        <div class="border"></div>
+                    </a>
+                </li>
+
+                <li>
+                    <a href="/">
+                        Movies
+                    </a>
+                </li>
+
+                <li>
+                    <a href="/">
+                        Tv
+                    </a>
+                </li>
+
+                <li>
+                    <a href="/">
+                        Games
+                    </a>
+                </li>
+
+                <li>
+                    <a href="/">
+                        Collectibles
+                    </a>
+                </li>
+
+                <li>
+                    <a href="/">
+                        Videos
+                    </a>
+                </li>
+
+                <li>
+                    <a href="/">
+                        Fans
+                    </a>
+                </li>
+
+                <li>
+                    <a href="/">
+                        News
+                    </a>
+                </li>
+
+                <li>
+                    <a href="/">
+                        Shop
+                    </a>
+                </li>
+            </ul>
+        </div>
+    </nav>
+
+    <div class="jumbotron"></div>
+</header>
